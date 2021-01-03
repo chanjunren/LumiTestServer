744e925a8e3e2b9ebe12b0502bd5bc41521df737
Jun Ren <chanjunren@gmail.com>
2021-01-03 20:48:46 +0800

    Added basic socket login logic in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-const {connectToDb} = require('./globals/db_globals');
-const {createNewExamManager, init} = require('./session_logic/exam_manager');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const {createNewExamManager, init, showStatsOf} = require('./session_logic/exam_manager');
 
 // Array of ExamManagers
 const managerMap = new Array();
@@ -11,7 +14,8 @@ function initiateExam(testAlias) {
     examManager.conn.once('connected', async function() {
         try {
           await init(examManager);
-          console.log("manager: ", examManager);
+          console.log("=== Initiated ===")
+          showStatsOf(examManager);
         }
         catch(e) {
           console.error(e);
--- src/session_logic/exam_manager.js
+++ src/session_logic/exam_manager.js
@@ -1,19 +1,34 @@
+
 const {getConnection} = require('../globals/db_globals');
 const {getAccountsModel} = require('../models/db_schemas');
 
 function createNewExamManager(testAlias) {
     const examManager = {
+        testAlias: testAlias,
         conn: getConnection(testAlias),
+        masterList: new Map(),
     }
     return examManager;    
 }
 
 async function init(examManager) {
-    examManager.masterList = await retrieveMasterList(examManager.conn);
+    let accounts = await retrieveMasterList(examManager.conn);
+    for (account of accounts) {
+        examManager.masterList.set(account.id, {
+            sessionIdx: account.sessionIdx,
+            pw: account.pw,
+            type: account.type
+        })
+    }
 }
 
 function retrieveMasterList(connection) {
     return getAccountsModel(connection).find({});
 }
 
-module.exports = {createNewExamManager, init};
+function showStatsOf(examManager) {
+    console.log("TEST: ", examManager.testAlias);
+    console.log("Master List:", examManager.masterList);
+}
+
+module.exports = {createNewExamManager, init, showStatsOf};
